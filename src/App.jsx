2c8d6b1fd579e4308c548e0790050958002fdbef
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-12-10 21:58:29 +0600

    Install: Shadcn UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./components/pages/Home/Home";
 import Footer from "./components/shared/footer/Footer";
 import Navbar from "./components/shared/navbar/Navbar";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div>
       <h1 className="text-green-600 font-bold text-xl">Tailwind is Working!</h1>
+      <Button>I'm a button</Button>
       <Navbar />
       <Home />
       <Footer />
