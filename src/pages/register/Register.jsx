8ef8e0596a4d8403a13b39780c5c05e43269ae8a
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-12-13 09:51:02 +0600

    change: env property name

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -47,7 +47,7 @@ const Register = () => {
 
         const imgbbResponse = await axios.post(
           `https://api.imgbb.com/1/upload?key=${
-            import.meta.env.VITE_IMGBB_API_KEY
+            import.meta.env.VITE_IMGBB_API
           }`,
           formData
         );
